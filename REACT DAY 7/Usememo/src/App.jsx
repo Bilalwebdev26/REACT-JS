9a7a90b78d3741624de92eb8fb611d64c197e4ee
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-10-25 07:33:57 -0700

    day 7 react useMemo

--- REACT DAY 7/Usememo/src/App.jsx
+++ REACT DAY 7/Usememo/src/App.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./App.css";
+import { useMemo } from "react";
 
 function App() {
-  return <>
-   
-  </>;
+  const [add, setAdd] = useState(0);
+  const [sub, setSub] = useState(0);
+  const addCount = () => {
+    setAdd(add + 1);
+  };
+  const subCount = () => {
+    setSub(sub - 1);
+  };
+  const multmemo = useMemo(  function multiply() {
+    console.log("--Multiply--")
+    return add * 100;
+  },[add])
+
+  return (
+    <>
+      <h1>Learning Usememo</h1>
+      {multmemo}
+      <br />
+      <button onClick={addCount}>Addition</button>
+      <span>Add : {add}</span>
+      <br />
+      <button onClick={subCount}>Subtraction</button>
+      <span>Subtract : {sub}</span>
+    </>
+  );
 }
 
 export default App;
