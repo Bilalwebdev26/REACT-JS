8ddd1da462af4d6e151c06f4354efdb7af45e3e8
bilalwebdev26 <f2022266833@umt.edu.pk>
2024-11-09 08:49:37 -0800

    Update Functionallity in redux todo 15

--- REACT DAY 15/redux Todo own/src/components/Todo.jsx
+++ REACT DAY 15/redux Todo own/src/components/Todo.jsx
@@ -1,24 +1,97 @@
-import React from "react";
+// import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { removeTodo, updateTodo } from "../redux/feature/Todo";
+// import { useState } from "react";
+
+// const Todo = () => {
+//   const dispatch = useDispatch();
+//   const todos = useSelector((state) => state.todos);
+//   const [todoMsg, setTodoMsg] = useState(todos.todos);
+//   const [istodoEditable, setIstodoEditable] = useState(false);
+//   return (
+//     <div>
+//       {todos.map((todo) => {
+//         return (
+//           <li key={todo.id}>
+//             {todo.message}
+//             <button
+//               onClick={() => {
+//                 dispatch(removeTodo(todo.id));
+//               }}
+//             >
+//               Delete
+//             </button>
+//             <button onClick={()=>{dispatch(updateTodo(todo.id))}}>Update</button>
+//           </li>
+//         );
+//       })}
+    
+//     </div>
+//   );
+// };
+
+// export default Todo;
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { removeTodo, updateTodo } from "../redux/feature/Todo";
+import { removeTodo, updateTodo, toggleComplete } from "../redux/feature/Todo";
 
 const Todo = () => {
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todos);
+  const [editableTodoId, setEditableTodoId] = useState(null);
+  const [todoMsg, setTodoMsg] = useState("");
+
   return (
     <div>
       {todos.map((todo) => {
+        const isEditable = editableTodoId === todo.id;
+        const handleSave = () => {
+          dispatch(updateTodo({ id: todo.id, message: todoMsg, complete: todo.complete }));
+          setEditableTodoId(null); // Make it read-only again
+        };
+
         return (
-          <li key={todo.id}>
-            {todo.message}
+          <li key={todo.id} className="flex items-center space-x-4">
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => dispatch(toggleComplete(todo.id))}
+              className="cursor-pointer"
+            />
+
+            {isEditable ? (
+              <input
+                type="text"
+                value={todoMsg}
+                onChange={(e) => setTodoMsg(e.target.value)}
+                className="border px-2 py-1 rounded outline-none"
+              />
+            ) : (
+              <span className={todo.complete ? "line-through text-gray-500" : ""}>
+                {todo.message}
+              </span>
+            )}
+
             <button
               onClick={() => {
-                dispatch(removeTodo(todo.id));
+                if (isEditable) {
+                  handleSave();
+                } else {
+                  setEditableTodoId(todo.id);
+                  setTodoMsg(todo.message); // Pre-fill with current message
+                }
               }}
+              className="bg-blue-500 text-white px-2 py-1 rounded"
+            >
+              {isEditable ? "Save" : "Edit"}
+            </button>
+
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="bg-red-500 text-white px-2 py-1 rounded"
             >
               Delete
             </button>
-            <button onClick={()=>{dispatch(updateTodo(todo.id))}}>Update</button>
           </li>
         );
       })}
--- REACT DAY 15/redux Todo own/src/redux/feature/Todo.js
+++ REACT DAY 15/redux Todo own/src/redux/feature/Todo.js
@@ -1,32 +1,71 @@
-import {createSlice,nanoid} from "@reduxjs/toolkit"
+// import {createSlice,nanoid} from "@reduxjs/toolkit"
+
+// const initialState = {
+//     todos:[]
+// }
+
+// export const todoSlice = createSlice({
+//     name:"todos",
+//     initialState,
+//     reducers:{
+//         addTodo:(state,action)=>{
+//               const Todo={
+//                 id:nanoid(),
+//                 message:action.payload
+//               }
+//              state.todos.push(Todo) 
+//         },
+//         removeTodo:(state,action)=>{
+//            state.todos = state.todos.filter((todo)=>(todo.id !== action.payload))
+//         },
+//         updateTodo:(state,action)=>{
+//             state.todos = state.todos.map((todo)=>(todo.id === action.payload ? {...todo,message}:todo))
+//         },
+//         // isCompleteTodo:(state,action)=>{
+
+//         // }
+//     }
+// })
+
+// export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+
+// export default todoSlice.reducer
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos:[]
-}
+    todos: []
+};
 
 export const todoSlice = createSlice({
-    name:"todos",
+    name: "todos",
     initialState,
-    reducers:{
-        addTodo:(state,action)=>{
-              const Todo={
-                id:nanoid(),
-                message:action.payload
-              }
-             state.todos.push(Todo) 
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                message: action.payload,
+                complete: false // Default to incomplete when added
+            };
+            state.todos.push(todo);
         },
-        removeTodo:(state,action)=>{
-           state.todos = state.todos.filter((todo)=>(todo.id !== action.payload))
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
         },
-        updateTodo:(state,action)=>{
-            state.todos = state.todos.map((todo)=>(todo.id === action.payload ? {...todo,message}:todo))
+        updateTodo: (state, action) => {
+            const { id, message } = action.payload;
+            state.todos = state.todos.map((todo) =>
+                todo.id === id ? { ...todo, message } : todo
+            );
         },
-        // isCompleteTodo:(state,action)=>{
-
-        // }
+        toggleComplete: (state, action) => {
+            const todoId = action.payload;
+            state.todos = state.todos.map((todo) =>
+                todo.id === todoId ? { ...todo, complete: !todo.complete } : todo
+            );
+        }
     }
-})
+});
 
-export const {addTodo,removeTodo,updateTodo} = todoSlice.actions
+export const { addTodo, removeTodo, updateTodo, toggleComplete } = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
